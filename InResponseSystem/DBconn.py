cda7a00c3472be9a339b8abf7bd6ea9e165fba06
khaled <83522218+khaled-alshanbari@users.noreply.github.com>
2023-04-18 09:24:06 +0300

    Update DBconn.py

--- InResponseSystem/DBconn.py
+++ InResponseSystem/DBconn.py
@@ -1,91 +1,60 @@
-import mysql.connector as mysqlconn
-from mysql.connector import pooling as pooling
-import streamlit as st
+import os
+import time
 
+import mysql.connector
+import streamlit
 from mysql.connector import Error
-from mysql.connector import pooling
+import pandas as pd
 
 
-usernames = []
-names = []
-passwords = []
-global fetchUsers 
-global connection_object 
-connection_object =None
-fetchUsers = []
-
-def create_conn():
+def create_conn(host_name='sql7.freemysqlhosting.net', user_name='sql7613171', user_password='Z7PbeZZAbW', db_name='sql7613171'):
+    connection = None
     try:
-        connection_pool = pooling.MySQLConnectionPool(pool_name="pynative_pool",
-                                                    pool_size=500,
-                                                    pool_reset_session=True,
-                                                    host='sql7.freemysqlhosting.net',
-                                                    database='sql7613171',
-                                                    user='sql7613171',
-                                                    password='Z7PbeZZAbW',
-                                                    port=3306)
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
 
-        print("Printing connection pool properties ")
-        print("Connection Pool Name - ", connection_pool.pool_name)
-        print("Connection Pool Size - ", connection_pool.pool_size)
-        # Get connection object from a pool
-        connection_object = connection_pool.get_connection()
-        # connection_object = connection_pool.shutdown()
+    return connection
 
-        if connection_object.is_connected():
-            db_Info = connection_object.get_server_info()
-            print("Connected to MySQL database using connection pool ... MySQL Server version on ", db_Info)
 
-            cursor = connection_object.cursor()
-            cursor.execute("select database();")
-            record = cursor.fetchone()
-            print("Your connected to - ", record)
+def execute_query(connection, query):
+    cursor = None
+    try:
+        cursor = connection.cursor()
+    except Exception:
+        os.system('service mysql start')
+        time.sleep(2)
+        cursor = connection.cursor()
 
-            
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
 
-            cursor.execute(""" SELECT `username` FROM `users`""")
-            recordUsers = cursor.fetchall()
-        
-            cursor.execute(""" SELECT `name` FROM `users` """)
-            recordNames = cursor.fetchall()
-            
-            cursor.execute(""" SELECT `password` FROM `users` """)
-            recordPass = cursor.fetchall()  
-            
-
-            for row in recordUsers:
-                row  = ''.join(letter for letter in row if letter.isalnum())
-                usernames.append(row)
-                
-            for row in recordNames:
-                # row  = ''.join(letter for letter in row if letter.isalnum())
-                names.append(row)
-            for row in recordPass:
-                row  = ''.join(letter for letter in row if letter.isalnum())
-                passwords.append(row)
-            #for users profiles:                     
-            # cursor.execute('SELECT id, username , name, email FROM users;')
-            # fetchUsers = cursor.fetchall() 
-            # for row in fetchUsers:
-            #     fetchUsers.append(row)
-        return  connection_object   
-
-    except Error as e:
-        print("Error while connecting to MySQL using Connection pool ", e)
-    finally:
-        # closing database connection.
-        if connection_object.is_connected():
-            cursor.close()
-            connection_object.close()
-            print("MySQL connection is closed")
-
-def close_conn():
-    connection_object = create_conn()
-    # closing database connection.
-    if connection_object.is_connected():
-        create_conn.cursor.close()
-        connection_object.close()
-        print("MySQL connection is closed")
+def read_query(connection, query):
+    cursor = None
+    try:
+        cursor = connection.cursor()
+        result = None
+    except Exception:
+        streamlit.info("Starting MySQL Server....")
+        os.system('service mysql start')
+        time.sleep(5)
+        cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
 
 def call_users(c):
     cursor = c.cursor()
